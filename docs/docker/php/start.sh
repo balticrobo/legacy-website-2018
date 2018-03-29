2470863385ecce1b013f77b6b5b7208fa10d86cb
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2018-03-29 23:56:06 +0200

    Docker: add MySQL container

--- docs/docker/php/start.sh
+++ docs/docker/php/start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+docker-php-ext-install pdo pdo_mysql
 docker-php-ext-configure opcache --enable-opcache
 docker-php-ext-install opcache
 pecl install xdebug
 docker-php-ext-enable xdebug
 
 apt-get update -yqq
-apt-get install -yqq zip git xdebug
+apt-get install -yqq zip git
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
