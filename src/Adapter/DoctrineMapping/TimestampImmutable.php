66936b502b32a38572d848128528448e38e5c745
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2018-03-29 23:58:36 +0200

    Symfony: implement Doctrine ORM

---
+++ src/Adapter/DoctrineMapping/TimestampImmutable.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace BalticRobo\Website\Adapter\DoctrineMapping;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class TimestampImmutable extends Type
+{
+    private const DOCTRINE_REPRESENTATION = 'timestamp_immutable';
+    private const MYSQL_REPRESENTATION = 'INT';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return self::MYSQL_REPRESENTATION;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return new \DateTimeImmutable("@{$value}");
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->getTimestamp();
+    }
+
+    public function getName()
+    {
+        return self::DOCTRINE_REPRESENTATION;
+    }
+}
