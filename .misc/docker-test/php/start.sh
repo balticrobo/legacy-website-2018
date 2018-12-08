12f23b80f746c589a0d7f949e969010b97292b18
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2018-12-08 17:44:45 +0100

    Upgrade Docker&Travis files

---
+++ .misc/docker-test/php/start.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function run {
+  echo "############ $1 ############" | tr '_' ' '
+  shift
+  eval $@
+  quit_when_fail
+}
+
+function quit_when_fail {
+  if [[ $? -ne 0 ]]; then
+    echo "> Task failed!"
+    exit 1
+  fi
+}
+
+run Install_vendors_from_Composer composer install
+run Lint_configs bin/console lint:yaml config
+run Lint_translations bin/console lint:yaml translations
+run Lint_templates bin/console lint:twig templates
+run PHP_CodeSniffer vendor/bin/phpcs -p
+run PHP_Code_Styler vendor/bin/php-cs-fixer fix --dry-run --diff -vvv
+run VarDump_Check "vendor/bin/var-dump-check --symfony src"
--- .misc/docker/php/start.sh
+++ .misc/docker/php/start.sh
@@ -2,7 +2,7 @@
 
 composer install
 
-/opt/docker/wait-for-it.sh mysql:3306 -t 600
+wait-for-it mysql:3306 -t 600
 bin/console doctrine:migrations:migrate --no-interaction
 
 php-fpm
