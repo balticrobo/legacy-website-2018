3244d05341041bb042bcb77574559008182dc445
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2019-04-10 19:33:16 +0200

    Change email header data

--- _mail/src/helpers/eventDetails.js
+++ _mail/src/helpers/eventDetails.js
@@ -5,9 +5,9 @@
  */
 module.exports = function(locale) {
   if (locale === 'pl') {
-    return '26 - 27 maja 2018r.<br/>GPNT, Trzy Lipy 3<br/>Gdańsk';
+    return '17 - 19 maja 2019r.<br/>GPNT, Trzy Lipy 3<br/>Gdańsk';
   } else if (locale === 'en') {
-    return 'May 26-27, 2018<br/>GPNT, Trzy Lipy 3<br/>Gdansk, POLAND';
+    return 'May 17-19, 2019<br/>GPNT, Trzy Lipy 3<br/>Gdansk, POLAND';
   }
 
   return '';
